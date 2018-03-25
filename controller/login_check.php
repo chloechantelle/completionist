1e3245c4b53ff31f8d26790153afc4a8f3b2bab3
chloe chantelle <chloechantellepotter@gmail.com>
2018-03-25 21:46:02 +1000

    :file_folder: Changed login process

--- controller/login_check.php
+++ controller/login_check.php
@@ -1,36 +1,44 @@
 <?php
+	session_start();
+	include '../model/db.php';
+	// include 'header.php';
+	// include 'navigation.php';
+?>
+<?php
 
 // search emails
-$select_sql = "select Username from users where Username = '" . $_POST['Username'] . "';";
+// $select_sql = "  select Email, Password from users where Email = ' " . $_POST['Email'] . " '  and Password = ' " . $_POST['Password'] . " '  ;";
 
-// insert user
-//$login_sql = "INSERT INTO `users`(`Username`, `Password`) VALUES ('" . $_POST['Username'] . "','" . $_POST['Password'] . "')";	
-
-// connection
-$conn = new PDO("mysql:host=localhost;dbname=books", 'root', '');
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// function to search for existing users
+$select_sql = "select * from users where Email = '" . $_POST['Email'] . "' and Password = '" . $_POST['Password'] . "' ;";		
 $stmt = $conn->prepare($select_sql);
 $stmt->execute();
-
-// debug
-
 $result = $stmt->fetchAll();
+
+// grab role for session variable
+$getquery = "   SELECT * FROM `users` where Email = '" . $_POST['Email'] . "' ";
+$stmt = $conn->prepare($getquery);
+$stmt->execute();
+$getresult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// set variables
+$email = ($_POST['Email']);
+$role = ($getresult['Role']);
 
 // if it exists
 if (count($result) ) {
 	// echo "Email exists!";
-	header('Location: ../model/ActiveRequests.php');
+	$_SESSION['LoggedIn'] = true;
+	$_SESSION['CurrentUser'] = $email;
+	$_SESSION['Role'] = $role;
+	echo "Correct login!";
+	header('Location: ../view/ActiveRequests.php');
 }
 
 // if it doesn't exist
 else {		
-	?><script>
-
-	window.alert("Incorrect username or password, try again!");
-	window.location.href = "../index.php";
-
-	</script>
-	<?php
+	echo "Incorrect!";
+	// header('HTTP/1.1 401 Unauthorized', true, 401);
 	// run function to show JS pop up for below message
 	// echo "Incorrect username or password!";
 	// header('Location: ../index.php');
