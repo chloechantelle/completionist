00be4fa9b594fc7aef7f046834daa30cf46c991a
chloe chantelle <chloechantellepotter@gmail.com>
2018-03-25 21:41:43 +1000

    :file_folder: Updated register process

--- controller/register_check.php
+++ controller/register_check.php
@@ -1,20 +1,60 @@
 <?php
-	include '../model/db.php';
-	include '../view/header.php';
-	// include 'navigation.php';
+session_start();
+include '../model/db.php';
+include '../view/header.php';
+    // include 'navigation.php';
 ?>
 
 
 <?php
 
-// search users
-$select_sql = "select Username from users where Username = '" . $_POST['Username'] . "';";
+// search for existing users
+$select_sql = "select * from users where Email = '" . $_POST['Email'] . "' and Password = '" . $_POST['Password'] . "' ;";      
+$stmt = $conn->prepare($select_sql);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+// grab role for session variable
+$getquery = "   SELECT * FROM `users` where Email = '" . $_POST['Email'] . "' ";
+$stmt = $conn->prepare($getquery);
+$stmt->execute();
+$getresult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// set variables
+$password = $_POST['Password'];
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+// var_dump($hashed_password);
+
+$email = ($_POST['Email']);
+$role = 'Customer';
+
+// if it exists
+if (count($result) ) {
+    echo "Email exists already!";   
+}
+
+// if it doesn't exist
+else {      
+
+// set variables
+    $_SESSION['LoggedIn'] = true;
+    $_SESSION['CurrentUser'] = $email;
+    $_SESSION['Role'] = $role;
 
 // insert user
-$login_sql = "INSERT INTO users(Username, Password) VALUES ('" . $_POST['Username'] . "','" . $_POST['Password'] . "')";
+    $register = "INSERT INTO users(Email, Password, Role) VALUES (
+    '" . $_POST['Email'] . "',
+    '$hashed_password',
+    'Customer'
+)";
+    $stmt = $conn->prepare($register);
+    $stmt->execute();
 
-$stmt = $conn->prepare($login_sql);
-$stmt->execute();
-header('Location: ../model/view_books.php');
+// redirect user    
+    echo'Registered!';
+    foreach ($_SESSION as $key=>$val)
+                  echo $key." : ".$val."<br/>";
+header('Refresh: 5; URL=../view/ActiveRequests.php');              
+}
 
 ?>
