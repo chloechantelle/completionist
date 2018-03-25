af6bcaa7140ef03f941f705c2b0f4a66b51ecd22
chloe chantelle <chloechantellepotter@gmail.com>
2018-03-25 21:41:00 +1000

    :squirrel: Created logout process

---
+++ controller/logout_process.php
@@ -0,0 +1,19 @@
+<?php	
+session_start();
+session_unset(); 
+session_destroy(); 
+include '../model/db.php';	
+include '../view/header.php';	
+include '../view/pubnav.php';
+?>
+
+<?php
+header('Refresh: 3; URL=../index.php');
+?>	
+
+<style><?php include '../view/style.css';?></style>	
+<script src="../view/javascript.js"></script>
+
+<div class="logout"><h1>Logged out! Redirecting you now.</h1>
+<p>Click here if you don't get redirected.</p>
+</div>
