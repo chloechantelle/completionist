ef40bdf36eda46067e7db14aca54cbbb11350f8e
chloe chantelle <chloechantellepotter@gmail.com>
2018-03-25 21:35:39 +1000

    :squirrel: Included second jQuery script

--- view/header.php
+++ view/header.php
@@ -6,7 +6,8 @@
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
     <link rel="stylesheet" href="view/style.css">   
-    <script src="scripts/js.js"></script> 
+    <script src="view/javascript.js"></script> 
 </head>
 <main>      
