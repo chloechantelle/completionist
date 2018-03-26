451bd7ab3bf84c064f8bd4d4729561fe818ae53d
chloechantelle <chloechantelle@users.noreply.github.com>
2018-03-26 16:41:14 +1000

    :squirrel: Changed attribute input names

--- view/login.php
+++ view/login.php
@@ -9,8 +9,8 @@
 <div class="login">
 	<h1>Log in to Books Database</h1>	
 	<form action="controller/login_check.php" method="post">		
-		<input required type="text" name="Username" placeholder="Username">
-		<input required type="password" name="password" placeholder="Password">
+		<input required type="text" name="Email" placeholder="Email">
+		<input required type="Password" name="Password" placeholder="Password">
 		<input type="submit" name="submit" value="Log in">
 	</form>
 <div class="notice">Don't have an account? <a href="view/register.php">Register here</a>.</div>	
