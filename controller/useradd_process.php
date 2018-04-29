4a89da5a0bd5ad40f52062c3c8f3b9f080e2fb3c
chloe chantelle <chloechantellepotter@gmail.com>
2018-04-29 19:28:38 +1000

    Initialized

---
+++ controller/useradd_process.php
@@ -0,0 +1,63 @@
+<?php
+header('Refresh: 3; URL=../view/ActiveRequests.php');
+?>
+
+<?php	
+// error_reporting(-1);
+session_start();
+include '../model/db.php';	
+include '../view/header.php';	
+include '../view/navigation.php';
+?>	
+<style><?php include '../view/style.css';?></style>	
+<script src="../view/javascript.js"></script>
+
+<?php
+
+// set variables
+$ID = $_SESSION['UserID'];
+
+// create contract
+$insert_sql = "INSERT INTO `contract` (`ContractID`, `PaymentDate`, `PaymentAmount`, `TimeGiven`, `Status`, `GameID`, `UserID`) 
+VALUES
+( NULL, '', '', '', 'Submitted', 
+'" . $_POST['GameID'] . "',
+'$ID'
+
+);";
+
+// '" . $_POST['UserID'] . "'
+
+$stmt = $conn->prepare($insert_sql);
+$stmt->execute();
+
+// test
+// print_r($_POST);  
+
+echo '
+<div class="message">
+<p>Submitted contract!</p>
+<p>Redirecting you back to contracts</p>
+</div>
+';
+
+// '" . $_POST['PaymentDate'] . "',
+// '" . $_POST['GameTitle'] . "',
+// '" . $_POST['PaymentAmount'] . "'
+
+// INSERT INTO 
+// contract(PaymentDate, PaymentAmount)
+// values ('4/4/4', '50');
+
+// insert into
+// games(GameTitle)
+// VALUES ('NameName')
+
+// $stmt = $conn->prepare($insert_sql);
+// $stmt->execute();
+
+?>
+
+<?php
+			include '../view/footer.php';
+?>	
