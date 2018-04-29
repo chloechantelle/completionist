e8d5a6369e2744fb22549b3e2a70ce14e8999cec
chloe chantelle <chloechantellepotter@gmail.com>
2018-04-29 19:53:45 +1000

    Echoed to $root to fix URL issue/Added nav dropdown

--- view/navigation.php
+++ view/navigation.php
@@ -1,60 +1,158 @@
 <body>
 
   <?php
-$role_session = $_SESSION['Role'];
+// include 'header.php';   
+// redirect to root folder
+$PHP_SELF=$_SERVER['PHP_SELF'];
+$Root='http://'.$_SERVER['HTTP_HOST'].'/completionist'.substr($PHP_SELF,0,strrpos($PHP_SELF,''));
 
-if($role_session == 'Customer') {
-  ?>
-  <style>
-  .hidden { 
-    display: none !important;
-     }
-     </style>
-     <?php
+if (isset($_SESSION['Role']) && $_SESSION['Role'] == 'Admin') {
+    ?>
+    <style>.administrator { display: inherit;}</style>
+    <style>.user { display: none !important;}</style>
+    <style>.both { display: inherit;}</style>
+
+    <style>.contact {display: inherit;}</style>
+    <style>.log { display: none !important; }</style>
+    <style>.submitupdate { display: inherit;}</style>    
+    <style>#download-button {display: none;}</style>
+
+    <?php
 }
-else{
-  ?><style>.hidden { display: inherit; }</style><?php
+elseif (isset($_SESSION['Role']) && $_SESSION['Role'] == 'Customer') {
+    ?>
+    <style>.administrator { display: none; }</style>
+    <style>.user { display: inherit;}</style>
+    <style>.both { display: inherit;}</style>
+
+    <style>.contact {display: inherit;}</style>
+    <style>.log { display: none;}</style>
+    <style>.submitupdate { display: none;}</style>   
+    <style>#download-button {display: none;}</style> 
+
+    <?php
 }
+else {
+   ?>
+   <style>.administrator { display: none !important; }</style>
+   <style>.user { display: none;}</style>
+   <style>.both { display: none;}</style>
+
+   <style>.log { display: inherit;}</style>
+   <style>.contact {display: none !important;}</style>
+   <style>.submitupdate { display: none;}</style>    
+   <?php
+}
+
 ?>
+
+<?php echo'
+
+<ul id="dropdown1" class="dropdown-content">
+<li><a class="both" href=" ';
+echo $Root;
+echo '/view/Settings.php"><i class="material-icons left">account_circle</i>
+Settings</a></li>
+
+<li><a class="user" href=" ';
+echo $Root;
+echo '/view/ActiveRequests.php"><i class="material-icons left">account_circle</i>
+Your Contracts</a></li>
+
+<li><a class="both" href=" ';
+echo $Root;
+echo '/controller/logout_process.php"><i class="material-icons left">exit_to_app</i>Logout</a></li>
+</ul>
 
 <div class="navbar-fixed">
   <nav>
     <div class="nav-wrapper">
-      <a href="../index.php" class="brand-logo">The Completionist</a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <a href=" ';
+      echo $Root;
+      echo '/index.php" class="brand-logo">The Completionist</a>';
+      echo'<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-        <li><a href="AdminProfile.php"><i class="material-icons left">account_circle</i>Completionist</a></li>
+<ul class="right hide-on-med-and-down">
 
-        <li><a href="ActiveRequests.php"><i class="material-icons left">event_note</i>Active Requests</a></li>
+      <li><a class="log modal-trigger" href="#login"><i class="material-icons left">account_circle</i>Login</a></li>';
+      echo'
 
-        <li class="hidden"><a href="Contract.php"><i class="material-icons left">add_circle_outline</i>Create Contract</a></li>
+      <li><a href=" ';
+      echo $Root;
+      echo '/view/AdminProfile.php"><i class="material-icons left">info_outline</i>About</a></li>';
+      echo' 
 
-        <li class="hidden"><a href="UpdateContract.php"><i class="material-icons left">update</i>Update Contract</a></li>
+      <li><a class="administrator" href=" ';
+      echo $Root;
+      echo '/view/ActiveRequests.php"><i class="material-icons left">event_note</i>Active Requests</a></li>';
 
-        <li><a href="../controller/logout_process.php"><i class="material-icons left">exit_to_app</i>Logout</a></li>
+$email = $_SESSION['CurrentUser'];
 
+      echo' 
 
-      </ul>
+     <li><a class="user" href=" ';
+      echo $Root;
+      echo '/view/Submit.php"><i class="material-icons left">add_circle_outline</i>Submit Contract</a></li>';
+      echo '
+
+      <li><a class="administrator" href=" ';
+      echo $Root;
+      echo '/view/Contract.php"><i class="material-icons left">add_circle_outline</i>Create Contract</a></li>
+      <li><a class="both dropdown-trigger" data-target="dropdown1"><i class="material-icons left">account_circle</i>
+      Welcome ' . $email . '!
+      <i class="material-icons right">arrow_drop_down</i></a></li>    
+    </ul>
     </div>
+    </nav>
+    
+<ul class="sidenav" id="mobile-demo">
 
-    <!-- mobile -->
-<ul class="side-nav" id="mobile-demo">
+      <li><a class="log modal-trigger" href=" ';
+      echo $Root;
+      echo '/index.php#login"><i class="material-icons left">account_circle</i>Login</a></li>';
+      echo'
 
-        <li><a href="view/AdminProfile.php"><i class="material-icons left">account_circle</i>Completionist</a></li>
+      <li><a href=" ';
+      echo $Root;
+      echo '/view/AdminProfile.php"><i class="material-icons left">account_circle</i>Completionist</a></li>';
+      echo' 
 
-        <li><a href="ActiveRequests.php"><i class="material-icons left">event_note</i>Active Requests</a></li>
+      <li><a class="user" href=" ';
+      echo $Root;
+      echo '/view/ActiveRequests.php"><i class="material-icons left">event_note</i>Active Requests</a></li>';
+      echo' 
 
-        <li class="hidden"><a href="Contract.php"><i class="material-icons left">add_circle_outline</i>Create Contract</a></li>
+      <li><a class="administrator" href=" ';
+      echo $Root;
+      echo '/view/Contract.php"><i class="material-icons left">add_circle_outline</i>Create Contract</a></li>';
+      echo' 
 
-        <li class="hidden"><a href="UpdateContract.php"><i class="material-icons left">update</i>Update Contract</a></li>
+      <li><a class="administrator" href=" ';
+      echo $Root;
+      echo '/view/UpdateContract.php"><i class="material-icons left">update</i>Update Contract</a></li>';
+      echo' 
 
-        <li><a href="../controller/logout_process.php"><i class="material-icons left">exit_to_app</i>Logout</a></li>
+      <li><a class="user" href=" ';
+      echo $Root;
+      echo '/controller/logout_process.php"><i class="material-icons left">exit_to_app</i>Logout</a></li>';
+      echo' 
 
 
       </ul>
-
   </nav>
 </div>  
+'?>
 
+<script>
+  $(document).ready(function(){
+    $(".dropdown-trigger").dropdown();
+  });
+</script> 
+
+<script>
+function register() {
+$('#login').modal();
+$('#register').modal();
+};
+</script>  
