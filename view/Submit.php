02a041468fab1838e7045f8430038f425c7d0105
chloe chantelle <chloechantellepotter@gmail.com>
2018-04-29 19:39:00 +1000

    Created seperate page for users to request contract

---
+++ view/Submit.php
@@ -0,0 +1,68 @@
+<?php	
+session_start();
+include '../model/db.php';  
+include 'header.php';
+include 'navigation.php';
+?>	
+<style><?php include '../view/style.css';?></style>	
+<script src="../view/javascript.js"></script>
+
+<?php
+$gameselect = " SELECT * FROM `games`";
+$stmt = $conn->prepare($gameselect);
+$stmt->execute();
+$gameresult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+$userselect = " SELECT * FROM `users`";
+$stmt = $conn->prepare($userselect);
+$stmt->execute();
+$userresult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<form name="registration" class="contract" action="../controller/useradd_process.php" method="post">
+
+<div class="input-field col s12 m6">
+
+<select name="GameID" class="icons">
+<option selected>Choose game</option>
+<?php 
+    foreach($gameresult as $game) {
+      echo'
+      <option value="' . $game['GameID'] . '" data-icon="../view/img/pachimari.png" class="circle">
+      ' . $game['GameTitle'] . '
+      </option>'      
+    ;}?>
+</select>
+
+<?php 
+$email = $_SESSION['CurrentUser'];
+$ID = $_SESSION['UserID'];
+?>
+
+<select name="UserID" class="icons" disabled>
+<?php
+echo'
+      <option value="' . $ID . '" data-icon="../view/img/pachimari.png" class="circle">
+      ' . $email . '
+      </option>'     
+?>  
+</select>
+
+
+  </div>
+
+       <p class="center">
+        Request will be submitted and then approved by an admin. Once approved you will be notified with a set price and estimated time it'll take to be completed. Once agreed upon and the payment has been recieved the completing can be started!
+       </p>
+
+       <div class="submit">
+	<input class="sub waves-effect waves-light btn-large" type="submit" name="submit" value="Submit Request!">
+      </div>
+
+	</form>
+
+<?php
+			include '../view/footer.php';
+?>	
