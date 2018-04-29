9e8566c4703a55e32916905c4b6eba6777790015
chloe chantelle <chloechantellepotter@gmail.com>
2018-04-29 19:43:14 +1000

    Updated Materialize cdn's :gun: :gun:

--- view/header.php
+++ view/header.php
@@ -3,10 +3,12 @@
     <link rel="icon" href="../view/img/pachimari.png">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+    <!-- <link rel="stylesheet" href="view/style.css">    -->
+    <!-- <script src="view/javascript.js"></script>  -->
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>  
     <link rel="stylesheet" href="view/style.css">   
     <script src="view/javascript.js"></script> 
 </head>
