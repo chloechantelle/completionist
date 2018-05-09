dc0ea3b8a3d8a447d1993882763291e8fe7e42dd
chloe chantelle <chloechantellepotter@gmail.com>
2018-05-09 21:28:08 +1000

    Made changes to validate function

--- view/javascript.js
+++ view/javascript.js
@@ -1,14 +1,100 @@
-// debug switch
-function switchfunction() {
-    var x = document.getElementById("debug");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        localStorage.setItem('debugswitch', 'on');
-    } else {
-        x.style.display = "none";
-        localStorage.setItem('debugswitch', 'off');
-    }
+$(document).ready(function(){
+    // $('.tooltipped').tooltip({delay: 50}); 
+    $('.modal').modal();
+    $('select').formSelect();
+    $('.scrollspy').scrollSpy();
+    $('.sidenav').sidenav();
+    // $('select').material_select();
+    $('.dropdown-trigger').dropdown();
+});
+
+ $(document).ready(function(){
+    $('.datepicker').datepicker({
+      format: 'dd/mm/yyyy',
+      defaultDate: '5/05/2018',
+      setDefaultDate: true,
+    },      
+      );
+  });
+
+function buttonfunction() {
+  settingsbutt.style.pointerEvents = "auto";
+  settingsbutt.style.cursor = "pointer";
+  settingsbutt.style.opacity = "0.8";
+  // settingsbutt.style.display = 'block';
+  // settingsbutt.removeAttribute('style');
 }
+
+// remember checkbox into localStorage        
+$(document).ready(function(){
+    
+var formValues = JSON.parse(localStorage.getItem('formValues')) || {};
+var $checkboxes = $("#checkbox-container :checkbox");
+var $button = $("#checkbox-container button");
+
+function allChecked(){
+  return $checkboxes.length === $checkboxes.filter(":checked").length;
+}
+
+function updateButtonStatus(){
+  $button.text(allChecked()? "Uncheck all" : "Check all");
+}
+
+function handleButtonClick(){
+  $checkboxes.prop("checked", allChecked()? false : true)
+}
+
+function updateStorage(){
+  $checkboxes.each(function(){
+    formValues[this.id] = this.checked;
+  });
+
+  formValues["buttonText"] = $button.text();
+  localStorage.setItem("formValues", JSON.stringify(formValues));
+}
+
+$button.on("click", function() {
+  handleButtonClick();
+  updateButtonStatus();
+  updateStorage();
+});
+
+$checkboxes.on("change", function(){
+  updateButtonStatus();
+  updateStorage();
+});
+
+// On page load
+$.each(formValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+$button.text(formValues["buttonText"]);
+
+  });
+
+// debug switch
+// function switchfunction() {
+//     var x = document.getElementById("debug");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+
+//         localStorage.setItem('debug', 'block');
+//     } else {
+//         x.style.display = "none";
+
+//         localStorage.setItem('debug', 'none');
+//     }
+// }
+
+// function debugOn() {
+//   settings.style.display = "block";
+// }
+
+// function debugOff() {
+//   debug.style.display = "block";
+// }
+
 
 // remember debug switch 
 // if (typeof(Storage) !== "undefined") {
@@ -28,7 +114,7 @@ function switchfunction() {
 $(function() {
   $("form[name='registration']").validate({
     rules: {
-      PaymentDate: "required",
+      Date: "required",
       PaymentAmount: "required",
       TimeGiven: {
         required: true
@@ -38,7 +124,7 @@ $(function() {
       }
     },
     messages: {
-      PaymentDate: "Please enter valid date",
+      Date: "Please enter valid date",
       Status: "Please enter valid status",
       PaymentAmount: "Please enter valid amount",
       TimeGiven: {
@@ -51,14 +137,6 @@ $(function() {
   });
 });
 
-  $(document).ready(function(){
-    $(".dropdown-trigger").dropdown();
-  });
-
-   $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
   // $('.dropdown-button').dropdown({
   //          inDuration: 300,
   //          outDuration: 225,
@@ -69,25 +147,15 @@ $(function() {
   //          }
   //     );
 
-$(document).ready(function(){
-    // $('.tooltipped').tooltip({delay: 50}); 
-    // $(".button-collapse").sideNav();
-    $('.modal').modal();
-    $('select').formSelect();
-    $('.scrollspy').scrollSpy();
-    // $('.sidenav').sidenav();
-    // $('select').material_select();
-    // $('.dropdown-trigger').dropdown();
-    // $('.dropdown-button').dropdown();
-    // $('.carousel').carousel();
-    // $('.scrollspy').scrollSpy();
-});
-
 console.log("beep");
 
 function register() {
 $('#login').modal();
 $('#register').modal();
+};
+
+function login() {
+$('#login').modal();
 };
 
  $(document).ready(function(){
