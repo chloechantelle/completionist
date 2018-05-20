fd6d17f37394ff0e41278f29b12cb9d9fabeb9ec
chloe chantelle <chloechantellepotter@gmail.com>
2018-05-20 15:30:27 +1000

    Added alt error modes

--- model/db.php
+++ model/db.php
@@ -1,5 +1,8 @@
 <?php
 	$conn = new PDO("mysql:host=localhost;dbname=completionist", 'root', '');
+	// stop script - throw error
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// show error, continue script
+	// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	return $conn;
 ?>
