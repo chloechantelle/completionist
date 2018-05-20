f835adf7b475715b8b6ff4eb36f53942cf8105ff
chloe chantelle <chloechantellepotter@gmail.com>
2018-05-20 15:23:37 +1000

    Created pay process

---
+++ controller/pay_process.php
@@ -0,0 +1,61 @@
+<?php
+header('Refresh: 2; URL=../view/requests.php');
+?>
+
+<?php	
+// error_reporting(-1);
+session_start();
+include '../model/db.php';	
+include '../view/header.php';	
+include '../view/navigation.php';
+?>	
+<style><?php include '../view/style.css';?></style>	
+<script src="../view/javascript.js"></script>
+
+<?php
+
+// $getselect = '
+// SELECT * FROM contract 
+// inner join users on contract.UserID = users.UserID
+// inner join games on contract.GameID = games.GameID
+// where contract.ContractID = ' . $_GET['ContractID'] . '
+// ';
+
+// $stmt = $conn->prepare($getselect);
+// $stmt->execute();
+// $getresulta = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// sanitize variables
+// $paysan = $_POST['PaymentAmount'];
+// $timesan = $_POST['TimeGiven'];
+
+// sanitize
+// $pay = filter_var($paysan, FILTER_SANITIZE_STRING);
+// $time = filter_var($timesan, FILTER_SANITIZE_STRING);
+
+// set variables
+// $status = $_POST['Status'];
+
+// update contract
+$update = "UPDATE contract SET
+Status = 'Confirmed and Payed' WHERE ContractID = ' " . $_GET['ContractID'] . " '
+";
+// ' " . $_GET['ContractID'] . " '
+$stmt = $conn->prepare($update);
+$stmt->execute();
+
+// test
+// print_r($_POST);  
+
+echo '
+<div class="message">
+<p>Confirmed contract!</p>
+<p>Redirecting you back to contracts</p>
+</div>
+';
+
+?>
+
+<?php
+			include '../view/footer.php';
+?>	
