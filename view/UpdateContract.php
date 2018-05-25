0745806ba9faa04f6517cd639b167d9600475834
chloe chantelle <chloechantellepotter@gmail.com>
2018-05-25 14:00:15 +1000

    Cleaned up and merged code

--- view/UpdateContract.php
+++ view/UpdateContract.php
@@ -1,39 +1,29 @@
-
-	<?php
-	session_start();
-	include 'header.php';
+<?php
+session_start();
+include 'header.php';
+include '../model/db.php';  
+include 'navigation.php';
 
 // if user isn't admin, redirect to login
 
 if (isset($_SESSION['Role']) && $_SESSION['Role'] == 'Admin') {
-  }
-else {
-  header("Location: ../index.php");
 }
-	
-	include '../model/db.php';  
-	include 'navigation.php';
-	?>
-	<!-- load CSS -->
-	<style>
-	<?php include '../view/style.css';?>
-		select {
-			display: inherit;
-		}
-		/* js file/materialize not working? */
-	</style>
-	<!-- load JS -->
-	<script src="../view/javascript.js"></script>
+else {
+	header("Location: ../index.php");
+}
+?>
+<!-- load CSS -->
+<style>
+<?php include '../view/style.css';?>
+</style>
 
-	<?php
+<?php
 
-	$getquery = '
-	SELECT * FROM contract 
-	inner join users on contract.UserID = users.UserID
-	inner join games on contract.GameID = games.GameID
-	-- inner join contractstatus on contract.ContractID = contractstatus.ContractID
-	-- where contract.ContractID = "132"
-	where contract.ContractID = ' . $_GET['ContractID'] . '
+$getquery = '
+SELECT * FROM contract 
+inner join users on contract.UserID = users.UserID
+inner join games on contract.GameID = games.GameID 
+where contract.ContractID = ' . $_GET['ContractID'] . '
 	';
 
 	$ContractID = $_GET['ContractID'];
@@ -42,9 +32,6 @@ else {
 	$stmt->execute();
 	$getresult = $stmt->fetch(PDO::FETCH_ASSOC);
 	?>	
-
-<!-- loading animation -->
-<!-- <div id="drawing"></div> -->
 
 	<?php
 
@@ -55,21 +42,24 @@ else {
 	<h4 class="center">Contract for ' . $getresult['FirstName'] . ' ' . $getresult['LastName'] . ' 
 	for ' . $getresult['GameTitle'] . '</h4>
 
-	<label>Payment Amount</label>
+		<label>Payment Amount</label>
 	<input value="' . $getresult['PaymentAmount'] . '" type="text" name="PaymentAmount" placeholder="Payment Amount">
 
 	<label>Time Given</label>
 	<input value="' . $getresult['TimeGiven'] . '" type="text" name="TimeGiven" placeholder="Time Given">
 
-        <label>Current Status</label>
-          <select id="Status" name="Status">
-          <option selected>' . $getresult['Status'] . '</option>
-          <option disabled></option>
-          <option>Awaiting Confirmation</option>
-          <option>Payment Recieved</option>
-          <option>In Progress</option>
-          <option>Completed</option>
-          </select>
+	<label>Current Status</label>
+
+	<div class="input-field col s12 m6">
+	<select id="Status" name="Status">
+	<option selected>' . $getresult['Status'] . '</option>
+	<option disabled></option>
+	<option>Awaiting Confirmation</option>
+	<option>Payment Recieved</option>
+	<option>In Progress</option>
+	<option>Completed</option>
+	</select>
+	</div>
 
 	</form>
 	<div class="submit">
@@ -80,56 +70,53 @@ else {
 	<p><a href="../view/requests.php">Return to contracts</a></p>
 	</div>
 
-      	</div>	
+	</div>	
 	';
 	// }
 	?>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/svg.js/2.6.4/svg.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script>
 
-<script>
+		$("#update").click(function(e) {
+			e.preventDefault();
+			var form = $('#updatecontract');
+			var formData = $(form).serialize();
 
-$("#update").click(function(e) {
-    e.preventDefault();
-    var form = $('#updatecontract');
-    var formData = $(form).serialize();
+			$("#loader").show();
+			$("#msg").hide();
 
-$("#loader").show();
-$("#msg").hide();
+			$.ajax({
+				type: "POST",
+				url: $(form).attr('action'),
+				data: formData,        
+				success: function(result) {
+			// alert('Updated');
+			$("#loader").hide();
+			$("#msg").show();
+		},
+		error: function(result) {
+			alert('Error');
+		}
+	});
+		});
 
-    $.ajax({
-        type: "POST",
-        url: $(form).attr('action'),
-        data: formData,        
-        success: function(result) {
-            // alert('Updated');
-            $("#loader").hide();
-            $("#msg").show();
-        },
-        error: function(result) {
-            alert('Error');
-        }
-    });
-});
+	</script>
 
-</script>
-
-<div id="loader" class="loader loader--style2" title="1">
-  <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     width="100px" height="100px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-  <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-    <animateTransform attributeType="xml"
-      attributeName="transform"
-      type="rotate"
-      from="0 25 25"
-      to="360 25 25"
-      dur="1.2s"
-      repeatCount="indefinite"/>
-    </path>
-  </svg>
+	<div id="loader" class="loader loader--style2" title="1">
+		<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		width="100px" height="100px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+		<path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
+			<animateTransform attributeType="xml"
+			attributeName="transform"
+			type="rotate"
+			from="0 25 25"
+			to="360 25 25"
+			dur="1.2s"
+			repeatCount="indefinite"/>
+		</path>
+	</svg>
 </div>
 
 <?php
-	include '../view/footer.php';
+include '../view/footer.php';
 ?>	
--- view/javascript.js
+++ view/javascript.js
@@ -1,163 +1,111 @@
 $(document).ready(function(){
-    // $('.tooltipped').tooltip({delay: 50}); 
+
+    // materialize
+
     $('.modal').modal();
     $('select').formSelect();
     $('.scrollspy').scrollSpy();
     $('.sidenav').sidenav();
-    // $('select').material_select();
+    $('.carousel').carousel();
     $('.dropdown-trigger').dropdown();
-});
-
- $(document).ready(function(){
-    $('.datepicker').datepicker({
-      format: 'dd/mm/yyyy',
-      defaultDate: '5/05/2018',
-      setDefaultDate: true,
-    },      
-      );
-  });
-
-function buttonfunction() {
-  settingsbutt.style.pointerEvents = "auto";
-  settingsbutt.style.cursor = "pointer";
-  settingsbutt.style.opacity = "0.8";
-  // settingsbutt.style.display = 'block';
-  // settingsbutt.removeAttribute('style');
-}
-
-// remember checkbox into localStorage        
-$(document).ready(function(){
     
-var formValues = JSON.parse(localStorage.getItem('formValues')) || {};
-var $checkboxes = $("#checkbox-container :checkbox");
-var $button = $("#checkbox-container button");
+    // material datepicker
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        defaultDate: '5/05/2018',
+        setDefaultDate: true,
+    });           
 
-function allChecked(){
-  return $checkboxes.length === $checkboxes.filter(":checked").length;
-}
+    // remember checkbox state into localStorage        
 
-function updateButtonStatus(){
-  $button.text(allChecked()? "Uncheck all" : "Check all");
-}
+    var formValues = JSON.parse(localStorage.getItem('formValues')) || {};
+    var $checkboxes = $("#checkbox-container :checkbox");
+    var $button = $("#checkbox-container button");
 
-function handleButtonClick(){
-  $checkboxes.prop("checked", allChecked()? false : true)
-}
+    function allChecked(){
+        return $checkboxes.length === $checkboxes.filter(":checked").length;
+    }
 
-function updateStorage(){
-  $checkboxes.each(function(){
-    formValues[this.id] = this.checked;
-  });
+    function updateButtonStatus(){
+        $button.text(allChecked()? "Uncheck all" : "Check all");
+    }
 
-  formValues["buttonText"] = $button.text();
-  localStorage.setItem("formValues", JSON.stringify(formValues));
-}
+    function handleButtonClick(){
+        $checkboxes.prop("checked", allChecked()? false : true)
+    }
 
-$button.on("click", function() {
-  handleButtonClick();
-  updateButtonStatus();
-  updateStorage();
+    function updateStorage(){
+        $checkboxes.each(function(){
+            formValues[this.id] = this.checked;
+        });
+
+        formValues["buttonText"] = $button.text();
+        localStorage.setItem("formValues", JSON.stringify(formValues));
+    }
+
+    $button.on("click", function() {
+        handleButtonClick();
+        updateButtonStatus();
+        updateStorage();
+    });
+
+    $checkboxes.on("change", function(){
+        updateButtonStatus();
+        updateStorage();
+    });
+
+    // On page load
+    $.each(formValues, function(key, value) {
+        $("#" + key).prop('checked', value);
+    });
+    $button.text(formValues["buttonText"]);
+
 });
 
-$checkboxes.on("change", function(){
-  updateButtonStatus();
-  updateStorage();
-});
-
-// On page load
-$.each(formValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
-
-$button.text(formValues["buttonText"]);
-
-  });
-
-// debug switch
-// function switchfunction() {
-//     var x = document.getElementById("debug");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-
-//         localStorage.setItem('debug', 'block');
-//     } else {
-//         x.style.display = "none";
-
-//         localStorage.setItem('debug', 'none');
-//     }
-// }
-
-// function debugOn() {
-//   settings.style.display = "block";
-// }
-
-// function debugOff() {
-//   debug.style.display = "block";
-// }
-
-
-// remember debug switch 
-// if (typeof(Storage) !== "undefined") {
-//     // store
-//     localStorage.setItem("state", "none");
-//     // retrieve
-//     document.getElementById("debug").innerHTML = localStorage.getItem("state");
-// } else {
-//     document.getElementById("debug").innerHTML = "browser does not support Web Storage";
-// }
-
-        // settings - change accent color
-//   localStorage.setItem('color', 'red');
-//   localStorage.getItem('color');
+// debug switch style
+function buttonfunction() {
+    settingsbutt.style.pointerEvents = "auto";
+    settingsbutt.style.cursor = "pointer";
+    settingsbutt.style.opacity = "0.8";
+}
 
 // validation
 $(function() {
-  $("form[name='registration']").validate({
-    rules: {
-      Date: "required",
-      PaymentAmount: "required",
-      TimeGiven: {
-        required: true
-      },
-      Status: {
-        required: true
-      }
-    },
-    messages: {
-      Date: "Please enter valid date",
-      Status: "Please enter valid status",
-      PaymentAmount: "Please enter valid amount",
-      TimeGiven: {
-        required: "Please enter valid time"
-      },
-    },
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
+    $("form[name='registration']").validate({
+        rules: {
+            Date: "required",
+            PaymentAmount: "required",
+            TimeGiven: {
+                required: true
+            },
+            Status: {
+                required: true
+            }
+        },
+        messages: {
+            Date: "Please enter valid date",
+            Status: "Please enter valid status",
+            PaymentAmount: "Please enter valid amount",
+            TimeGiven: {
+                required: "Please enter valid time"
+            },
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
 });
 
-  // $('.dropdown-button').dropdown({
-  //          inDuration: 300,
-  //          outDuration: 225,
-  //          constrain_width: true, 
-  //          hover: false, 
-  //          gutter: 0, 
-  //          belowOrigin: false 
-  //          }
-  //     );
-
+// test beep
 console.log("beep");
 
+// hide login then show register modal
 function register() {
-$('#login').modal();
-$('#register').modal();
+    $('#login').modal();
+    $('#register').modal();
 };
 
+// show login modal
 function login() {
-$('#login').modal();
+    $('#login').modal();
 };
-
- $(document).ready(function(){
-    $('.carousel').carousel();
-  });
